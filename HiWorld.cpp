90034f7f8296233d556e4ba4ccec73451222395d
Nguyen-Gia-Bao <giabao20924@gmail.com>
2023-07-03 20:43:29 +0700

    Git tutorial

--- HiWorld.cpp
+++ HiWorld.cpp
@@ -10,4 +10,5 @@
 // Git checkout <name branch>
 // Git remote add <name url> url
 // Git push <name url> <name branch>
+// cd duong-dan -> git clone url -> cd thu-muc-project -> code .
 // Tutorial: https://www.youtube.com/watch?v=QE_mkDiu4hE
