48c4147a10ac9468a4a159d2f579c38100a54d31
Nguyen-Gia-Bao <giabao20924@gmail.com>
2023-07-03 19:22:26 +0700

    test

--- HiWorld.cpp
+++ HiWorld.cpp
@@ -1,5 +1,11 @@
-// Git init
-// Git status 
-// Git add 
-// Git reset 
-
+// Git init 
+// Git status
+// Git add .
+// Git commit -m '%'
+// Git log 
+// Git log --oneline
+// Git merge 
+// Git branch
+// Git checkout -b <name branch>
+// Git checkout <name branch>
+// Git push url 
